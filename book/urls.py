28475cf7a9819bb89fdee57d4bc7a3e73473f373
amirhosseineilat <amirhosseineilat@gmail.com>
2026-08-28 15:32:08 +0330

    Refactor URL patterns in book app for improved readability

--- book/urls.py
+++ book/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('List/',views.book_list_view,name='book-List'),
+    path('Add/',views.book_add_view,name='book-Add'),
+    path('Search/',views.book_search_view,name='book-Add'),
+    path('Edit/',views.book_edit_view,name='book-Edit'),
+    path('Delete/',views.book_delete_view,name='book-Delete'),
+    path('Filter/',views.book_filter_view,name='book-Filter'),
 ]
