5bd45c5f06edfe04182819b28b8dec5b98308ad5
amirhosseineilat <amirhosseineilat@gmail.com>
2026-08-28 15:10:04 +0330

    Add models for Category and Book with fields for title, author, ISBN, publisher, publish date, price, page count, favorites, and genre

--- book/models.py
+++ book/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=100)
+    publisher = models.CharField(max_length=100)
+    publish_date = models.DateField()
+    price = models.FloatField()
+    page = models.IntegerField()
+    favorites = models.ManyToManyField(User, related_name="favorites")
+    genre = models.ManyToManyField(User, related_name="categories")
+
+
